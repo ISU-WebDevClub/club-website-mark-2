42afdb80fa53c11072c0d6b936cce80b903cbddf
Gregory Steenhagen <gregory@Gregorys-iMac.local>
2016-09-12 19:13:43 -0500

    Admin page javascript is set up. Switches between divs now so we can update the relevant page

--- admin/admin.js
+++ admin/admin.js
@@ -1,18 +1,79 @@
 /**
  * Created by gregory on 9/11/16.
  */
-var home = document.getElementById('home_page');
-var about = document.getElementById('about_page');
-var portfolio = document.getElementById('portfolio_page');
-var members = document.getElementById('members_page');
-var resources = document.getElementById('resources_page');
-var contact =document.getElementById('contact_page');
+var home = document.getElementById('home_tab');
+var about = document.getElementById('about_tab');
+var portfolio = document.getElementById('portfolio_tab');
+var members = document.getElementById('members_tab');
+var resources = document.getElementById('resources_tab');
+var contact =document.getElementById('contact_tab');
 
+var home_page = document.getElementById('home_page');
+var about_page = document.getElementById('about_page');
+var portfolio_page = document.getElementById('portfolio_page');
+var members_page = document.getElementById('members_page');
+var resources_page = document.getElementById('resources_page');
+var contact_page =document.getElementById('contact_page');
 
 home.onclick = function(){
-  select_tab('home_page');
+  select_home();
+};
+about.onclick = function(){
+  select_about();
+};
+portfolio.onclick = function(){
+  select_portfolio();
+};
+members.onclick = function(){
+  select_members();
+};
+resources.onclick = function(){
+  select_resources();
+};
+contact.onclick = function(){
+  select_contact();
 };
 
-function select_tab(string){
-    
+function select_home(){
+  remove_selected();
+  home.classList.add('selected_tab');
+  home_page.classList.add('selected_div');
 }
+
+function select_about(){
+  remove_selected();
+  about.classList.add('selected_tab');
+  about_page.classList.add('selected_div');
+}
+
+function select_portfolio(){
+  remove_selected();
+  portfolio.classList.add('selected_tab');
+  portfolio_page.classList.add('selected_div');
+}
+
+function select_members(){
+  remove_selected();
+  members.classList.add('selected_tab');
+  members_page.classList.add('selected_div');
+}
+
+function select_resources(){
+  remove_selected();
+  resources.classList.add('selected_tab');
+  resources_page.classList.add('selected_div');
+}
+
+function  select_contact() {
+  remove_selected();
+  contact.classList.add('selected_tab');
+  contact_page.classList.add('selected_div');
+}
+
+function remove_selected(){
+  var selected = document.getElementsByClassName("selected_tab");
+  selected[0].classList.remove('selected_tab');
+  var selected_div = document.getElementsByClassName("selected_div");
+  selected_div[0].classList.remove('selected_div');
+}
+
--- admin/index.php
+++ admin/index.php
@@ -28,33 +28,47 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../includes/css/header.css">
     <link rel="stylesheet" href="../includes/css/footer.css">
+
 </head>
 <?php include "../includes/php/header.php" ?>
 <body>
 <div id="content">
+
     <div id="content-sub">
-        <div id="home_page" class="tab_item">
-
+        <h1>Admin Page</h1>
+        <hr>
+        <div id="menu_grid">
+            <h2 class="tab_item selected_tab" id="home_tab">Home</h2>
+            <h2 class="tab_item" id="about_tab">About</h2>
+            <h2 class="tab_item" id="portfolio_tab">Portfolio</h2>
+            <h2 class="tab_item" id="members_tab">Members</h2>
+            <h2 class="tab_item" id="resources_tab">Resources</h2>
+            <h2 class="tab_item" id="contact_tab">Contact</h2>
         </div>
-        <div id="about_page" class="tab_item" hidden>
-
+        
+        <div id="home_page" class="tab_item_div selected_div">
+            <h1>Home Page</h1>
         </div>
-        <div id="portfolio_page" class="tab_item" hidden>
-
+        <div id="about_page" class="tab_item_div" hidden>
+            <h1>About Page</h1>
         </div>
-        <div id="members_page" class="tab_item" hidden>
-
+        <div id="portfolio_page" class="tab_item_div" hidden>
+            <h1>Portfolio Page</h1>
         </div>
-        <div id="resources_page" class="tab_item" hidden>
-
+        <div id="members_page" class="tab_item_div" hidden>
+            <h1>Members Page</h1>
         </div>
-        <div id="contact_page" class="tab_item" hidden>
-
+        <div id="resources_page" class="tab_item_div" hidden>
+            <h1>Resources Page</h1>
+        </div>
+        <div id="contact_page" class="tab_item_div" hidden>
+            <h1>Contact Page</h1>
         </div>
 
 
     </div>
 </div>
+<script src="admin.js" type="application/javascript"></script>
 </body>
 </html>
 
