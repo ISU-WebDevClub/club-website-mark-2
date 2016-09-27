397c9c7315c378ddc5fa972c9fdf59c8d69451ce
Gregory Steenhagen <gregory@Gregorys-iMac.local>
2016-09-27 08:27:48 -0500

    Fixed members page taphover

--- members/index.php
+++ members/index.php
@@ -77,7 +77,18 @@ $query = mysqli_query($conn, $sql);
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="application/javascript"></script>
 
 <script>
-    d
+    $('a.taphover').on('touchend', function (e) {
+        'use strict'; //satisfy code inspectors
+        var link = $(this); //preselect the link
+        if (link.hasClass('hover')) {
+            return true;
+        } else {
+            link.addClass('hover');
+            $('a.taphover').not(this).removeClass('hover');
+            e.preventDefault();
+            return false; //extra, and to make sure the function has consistent return points
+        }
+    });
 </script>
 </html>
 
